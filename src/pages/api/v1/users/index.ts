387244efb18770587977be13c3d4304dda0fd79f
Fernando López <ferlopezm94@gmail.com>
2020-07-25 18:56:22 -0500

    feat(users): add create and get many handlers

--- src/pages/api/v1/users/index.ts
+++ src/pages/api/v1/users/index.ts
@@ -1,7 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json('Hello users');
+import { connectToDatabase } from './../../../../utils';
+import { create, getMany } from './../../../../v1/users/controller';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.info({ status: 'handler start' });
+
+  await connectToDatabase();
+
+  switch (req.method) {
+    case 'POST':
+      return create(req, res);
+    case 'GET':
+      return getMany(req, res);
+    default:
+      console.warn({ status: `Method ${req.method} Not Allowed` });
+      return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 };
 
 export default handler;
