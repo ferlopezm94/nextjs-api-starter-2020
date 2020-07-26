b259b1a017b244fbf66e1ac4357c88df8235de35
Fernando López <ferlopezm94@gmail.com>
2020-07-25 19:05:29 -0500

    feat(logs): add logging support with pino as middleware

--- src/v1/users/controller.ts
+++ src/v1/users/controller.ts
@@ -3,26 +3,26 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { UserModel } from './model';
 
 export const create = async (req: NextApiRequest, res: NextApiResponse) => {
-  console.info({ status: 'create start', args: req.body });
+  req.logger.info({ status: 'create start', args: req.body });
 
   try {
     const user = new UserModel(req.body);
     await user.save();
     return res.status(201).json(user);
   } catch (error) {
-    console.error({ status: 'create error', error: error.message });
+    req.logger.error({ status: 'create error', error: error.message });
     return res.status(400).json({ error: error.message });
   }
 };
 
-export const getMany = async (_req: NextApiRequest, res: NextApiResponse) => {
-  console.info({ status: 'getMany start' });
+export const getMany = async (req: NextApiRequest, res: NextApiResponse) => {
+  req.logger.info({ status: 'getMany start' });
 
   try {
     const users = await UserModel.find({});
     return res.status(200).json(users);
   } catch (error) {
-    console.error({ status: 'getMany error', error: error.message });
+    req.logger.error({ status: 'getMany error', error: error.message });
     return res.status(500).json({ error: error.message });
   }
 };
